9f12aabe0cde24837bbf700dd98729e9152ff0d0
Wendy Fgigueroa <camomila444@gmail>
2021-02-10 18:15:03 -0500

    started on page db

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,13 @@
 ### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+CREATE DATABASE burger_db;
 
-CREATE TABLE cats
+USE burger_db;
+
+CREATE TABLE burger
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-INSERT INTO cats (name) VALUES ('Molly');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('');
+
 INSERT INTO cats (name) VALUES ('Charlie');
 INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
 INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
