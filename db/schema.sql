594906f8055078d24c1b75acac03ef56741d95ab
Wendy Fgigueroa <camomila444@gmail>
2021-02-11 10:37:49 -0500

    working on files

--- db/schema.sql
+++ db/schema.sql
@@ -10,5 +10,6 @@ CREATE TABLE burgers (
 	id INT NOT NULL AUTO_INCREMENT,
 	burger_name VARCHAR (50) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	createAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
